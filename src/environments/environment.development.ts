a11bf4b34a8265542bc49615c451947ab7b0b6a3
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2025-03-26 20:24:32 -0400

    gifs history

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,9 @@
 export const environment = {
 
-  production: false,
+  production: true,
   companyName: 'Gifs',
-  companySlogan: 'Manage your gifs'
+  companySlogan: 'Manage your gifs',
 
+  apiKey: 'zGtXGYt6XMPHEbzKj1cmOdtQ6Wb96YXL',
+  giphyUrl: 'https://api.giphy.com/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,8 @@ export const environment = {
 
   production: true,
   companyName: 'Gifs',
-  companySlogan: 'Manage your gifs'
+  companySlogan: 'Manage your gifs',
 
+  apiKey: 'zGtXGYt6XMPHEbzKj1cmOdtQ6Wb96YXL',
+  giphyUrl: 'https://api.giphy.com/v1'
 };
