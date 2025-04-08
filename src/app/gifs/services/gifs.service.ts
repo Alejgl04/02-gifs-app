3c12057100c6876ea0228f022358759f42fe9934
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2025-04-08 09:00:14 -0400

    keeping scroll state

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -25,7 +25,18 @@ export class GifService {
   private http = inject(HttpClient);
 
   trendingGifs = signal<Gif[]>([]);
-  tredingGifsLoading = signal(true);
+  tredingGifsLoading = signal(false);
+
+  private trendingPage = signal(0);
+
+  trendingGifGroup = computed<Gif[][]>(() => {
+    const groups = [];
+
+    for ( let i = 0; i < this.trendingGifs().length; i+=3 ) {
+      groups.push( this.trendingGifs().slice(i, i + 3));
+    }
+    return groups;
+  })
 
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
   searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
@@ -40,21 +51,26 @@ export class GifService {
   });
 
   loadTrendingGifs() {
+
+    if ( this.tredingGifsLoading() ) return;
+
+    this.tredingGifsLoading.set(true);
+
     this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
       params: {
         api_key: `${environment.apiKey}`,
         limit: 20,
+        offset: this.trendingPage() * 20,
       },
     }).subscribe( ( resp ) => {
-
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
-      this.trendingGifs.set(gifs);
+      this.trendingGifs.update(currentGifs => [...currentGifs, ...gifs]);
+      this.trendingPage.update((currentPage) => currentPage + 1);
       this.tredingGifsLoading.set(false);
-      console.log(gifs);
     });
   }
 
-  searchGifs( query:string ): Observable<Gif[]> {
+  searchGifs( query:string): Observable<Gif[]> {
     return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
       params: {
         api_key: `${environment.apiKey}`,
